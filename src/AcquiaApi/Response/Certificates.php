353e15390b03ee5270e5e80492561e5126b3d12e
starr027 <starr027@umn.edu>
2023-12-05 11:19:50 -0600

    Add function to generate CSR

--- src/AcquiaApi/Response/Certificates.php
+++ src/AcquiaApi/Response/Certificates.php
@@ -11,6 +11,11 @@ class Certificates extends AcquiaResponse {
    */
   protected $id;
 
+  /**
+   * @var array
+   */
+  protected $csr_data;
+
   public function __construct($response, Client $client)
   {
     parent::__construct($response, $client);
@@ -24,6 +29,17 @@ class Certificates extends AcquiaResponse {
     return $this->id;
   }
 
+  public function getCSRData(): array
+  {
+    return [
+      "country" => "US",
+      "state" => "Minnesota",
+      "locality" => "Minneapolis",
+      "organization" => "University of Minnesota",
+      "organizational_unit" => "OIT",
+    ];
+  }
+
   public function getCSR($id) {
     $uri = "environments/{$this->id}/ssl/csrs";
     $options = [
@@ -33,5 +49,21 @@ class Certificates extends AcquiaResponse {
     ];
     $response = $this->client->getRequest($uri, $options);
     return $response->getBody()->getContents();
+  }
+
+
+  public function generateCSR($id, $common_name, $csr_alternate_names=[]) {
+    $uri = "environments/{$this->id}/ssl/csrs";
+    $csr_data = $this->getCSRData();
+    $csr_data["common_name"] = $common_name;
+    $csr_data["alternate_names"] = $csr_alternate_names;
+    $options = [
+      'headers' => [
+        'Content-Type' => 'application/hal+json',
+      ],
+      'form_params' => $csr_data,
+    ];
+    $response = $this->client->postRequest($uri, $options);
+    return new AcquiaResponse($response, $this->client);
   }
 }
